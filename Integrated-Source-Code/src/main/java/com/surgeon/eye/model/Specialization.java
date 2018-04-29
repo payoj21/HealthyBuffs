132286df62c869c349219cf528d6197d10d6e7fb
Rahul Chowdhury <chowdhuryrahul5@gmail.com>
2018-04-28 22:34:33 -0600

    added specialization class

---
+++ Integrated-Source-Code/src/main/java/com/surgeon/eye/model/Specialization.java
@@ -0,0 +1,13 @@
+package com.surgeon.eye.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Specialization {
+
+    private List<Specialization> specialization;
+    
+
+}
